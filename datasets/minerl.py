fe0252d93640477e452eb06f2edb4de5353c2250
Saeid Naderiparizi <s.naderiparizi@gmail.com>
2022-04-28 16:49:54 -0700

    Add MineRL dataset processing script

--- datasets/minerl.py
+++ datasets/minerl.py
@@ -1,19 +1,26 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import minerl_navigate
 import os
+import numpy as np
+from pathlib import Path
 
-data_dir = os.path.dirname(os.path.abspath(__file__))
+# train = train.flat_map(lambda x: tf.data.Dataset.from_tensor_slices(
+#     tf.reshape(x['video'], (500, 64, 64, 3))))
 
-dataset = tfds.load('minerl_navigate', shuffle_files=True, data_dir=data_dir)
+if __name__ == "__main__":
+    data_dir = Path(os.path.dirname(os.path.abspath(__file__)))
+    orig_dataset = 'minerl_navigate'
+    torch_dataset_path = data_dir / f'{orig_dataset}-torch'
+    torch_dataset_path.mkdir(exist_ok=True)
 
-test = dataset['test']
+    for split in ['train', 'test']:
+        torch_split_path = torch_dataset_path / split
+        torch_split_path.mkdir(exist_ok=True)
 
-train = dataset['train'].repeat(2)# Repeats for the number of epochs
-train = train.flat_map(lambda x: tf.data.Dataset.from_tensor_slices(
-    tf.reshape(x['video'], (5, 100, 64, 64, 3))))
-train = train.shuffle(5000).batch(50).prefetch(1)
+        ds = tfds.load("minerl_navigate", data_dir=str(data_dir), shuffle_files=False)[split]
+        for cnt, item in enumerate(ds):
+            video = item["video"].numpy()
+            np.save(torch_split_path / f"{cnt}.npy", video)
 
-for batch in train:
-  assert batch.shape == (50, 100, 64, 64, 3)
-  assert batch.dtype == tf.uint8
-  break
+        print(f' [-] {cnt} scenes in the {split} dataset')
