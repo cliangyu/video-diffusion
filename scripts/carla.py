db8ce2902762149e0550a808fe87fae98ca3225f
William Harvey <wsgh@submit.cs.ubc.ca>
2022-05-06 16:36:27 -0700

    new carla script

--- scripts/carla.py
+++ scripts/carla.py
@@ -9,6 +9,23 @@ import imageio
 import matplotlib.pyplot as plt
 import argparse
 import torch
+
+# from https://stackoverflow.com/questions/2281850/timeout-function-if-it-takes-too-long-to-finish  --------------------------------------------------------------
+import signal
+
+class timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+# --------------------------------------------------------------
+
 IN_COLAB = False
 
 def_res = Resolution(128, 128)
@@ -17,8 +34,6 @@ scale = 1
 parser = argparse.ArgumentParser()
 parser.add_argument('save_dir', type=str, help="Where to save the generated videos/coords.")
 parser.add_argument('--port', type=int, default=5555, help="5555 is the server with other cars. Currently there are no other servers")
-parser.add_argument('--start_index', type=int, default=0)
-parser.add_argument('--n_indices', type=int, default=5)
 args = parser.parse_args()
 
 sensors_dict = {
@@ -42,7 +57,7 @@ config = fake_parser.parse_args(['--client_id', 'mycompany', '--enable_progress_
 server_ip = "simulate.inverted.ai"
 config.zmq_server_address = f"{server_ip}:{args.port}"
 env = IAIEnv(config)
-world_parameters = dict(carlatown='Town01')
+world_parameters = dict(carlatown='Town01', traffic_count=0, pedestrian_count=0, weather='Random')
 
 obs = env.set_scenario('egodriving', world_parameters=world_parameters, sensors=sensors_dict)
 
@@ -58,29 +73,45 @@ def reset_frames():
     return {'images':[], 'coords': [], 'actions': []}
 frames = reset_frames()
 
-T = 1000
+videos_per_trajectory = 10
+video_length = 5
+
+def get_save_name(index, mode='video', ext='pt'):
+    return os.path.join(args.save_dir, f'{mode}_{index}.{ext}')
+
+def next_save_index():
+    i = 0
+    while os.path.exists(get_save_name(i)):
+        i += videos_per_trajectory
+    return i
+
+trajectory_index = next_save_index()
+
 walltime = time.time()
-for i in range(0, T*args.n_indices):
-    action = info['expert_action']
-    obs, reward, done, info = env.step(action)
-    frames['images'].append(obs['sensor_data']['front-cam']['image'])
-    frames['coords'].append(obs['compact_vector'])
-    frames['actions'].append(action)
-
-    if i % T == T-1:
-        i_save = args.start_index + i//T
+for i in range(0, video_length*videos_per_trajectory):
+    try:
+        with timeout(seconds=10):
+            action = info['expert_action']
+            obs, reward, done, info = env.step(action)
+            frames['images'].append(obs['sensor_data']['front-cam']['image'])
+            frames['coords'].append(obs['compact_vector'])
+            frames['actions'].append(action)
+    except TimeoutError:
+        print('\n\nTimed out!!!!!! Exiting.\n\n')
+        exit()
+    if (i+1) % video_length == 0:
+        save_index = trajectory_index + i//video_length
+        # save
         video = torch.stack([torch.tensor(img) for img in frames['images']])
-        torch.save(video, os.path.join(args.save_dir, f'video_{i_save}.pt'))
-        imageio.mimwrite(os.path.join(args.save_dir, f'video_{i_save}.mp4'), frames['images'], fps=10, quality=7)
+        torch.save(video, get_save_name(save_index, 'video', 'pt'))
+        imageio.mimwrite(get_save_name(save_index, 'video', 'mp4'), frames['images'], fps=10, quality=7)
         coords = np.array(frames['coords'])
-        np.save(os.path.join(args.save_dir, f'coords_{i_save}.npy'), coords)
+        np.save(get_save_name(save_index, 'coords', 'npy'), coords)
         actions = np.array(frames['actions'])
-        np.save(os.path.join(args.save_dir, f'actions_{i_save}.npy'), coords)
+        np.save(get_save_name(save_index, 'actions', 'npy'), coords)
         frames = reset_frames()
-        print(f'generated {T} frames in {time.time()-walltime} seconds')
+        print(f'generated {video_length} frames in {time.time()-walltime} seconds')
         walltime = time.time()
-
-print('Done')
 
 print(env.end_simulation())
 env.close()
