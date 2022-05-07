131e0e66baa9caf234c3b2b03645fdbed071eb1e
William Harvey <wsgh@submit.cs.ubc.ca>
2022-05-06 17:09:54 -0700

    final carla data generation script

--- scripts/carla.py
+++ scripts/carla.py
@@ -26,55 +26,56 @@ class timeout:
         signal.alarm(0)
 # --------------------------------------------------------------
 
-IN_COLAB = False
+with timeout(seconds=1800):
+    IN_COLAB = False
 
-def_res = Resolution(128, 128)
-scale = 1
+    def_res = Resolution(128, 128)
+    scale = 1
 
-parser = argparse.ArgumentParser()
-parser.add_argument('save_dir', type=str, help="Where to save the generated videos/coords.")
-parser.add_argument('--port', type=int, default=5555, help="5555 is the server with other cars. Currently there are no other servers")
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('save_dir', type=str, help="Where to save the generated videos/coords.")
+    parser.add_argument('--port', type=int, default=5555, help="5555 is the server with other cars. Currently there are no other servers")
+    args = parser.parse_args()
 
-sensors_dict = {
-    'front-cam': {
-        'sensor_type': 'camera',
-        'camera_type': 'rgb-camera',
-        'bounding_box': False,
-        'track_actor_types': SensorSettings.Available_Tracked_Actors,
-        'show_bounding_boxes': False,
-        'world_sensor': False,
-        'resolution': def_res,
-        'location': SensorSettings.Location(x=2, z=2, y=0),
-        'rotation': SensorSettings.Rotation(yaw=0, roll=0, pitch=0),
-        'fov': 120.0,
-    },
-}
+    sensors_dict = {
+        'front-cam': {
+            'sensor_type': 'camera',
+            'camera_type': 'rgb-camera',
+            'bounding_box': False,
+            'track_actor_types': SensorSettings.Available_Tracked_Actors,
+            'show_bounding_boxes': False,
+            'world_sensor': False,
+            'resolution': def_res,
+            'location': SensorSettings.Location(x=2, z=2, y=0),
+            'rotation': SensorSettings.Rotation(yaw=0, roll=0, pitch=0),
+            'fov': 120.0,
+        },
+    }
 
-fake_parser = argparse.ArgumentParser()
-IAIEnv.add_config(fake_parser)
-config = fake_parser.parse_args(['--client_id', 'mycompany', '--enable_progress_spinner', '0'])
-server_ip = "simulate.inverted.ai"
-config.zmq_server_address = f"{server_ip}:{args.port}"
-env = IAIEnv(config)
-world_parameters = dict(carlatown='Town01', traffic_count=0, pedestrian_count=0, weather='Random')
+    fake_parser = argparse.ArgumentParser()
+    IAIEnv.add_config(fake_parser)
+    config = fake_parser.parse_args(['--client_id', 'mycompany', '--enable_progress_spinner', '0'])
+    server_ip = "simulate.inverted.ai"
+    config.zmq_server_address = f"{server_ip}:{args.port}"
+    env = IAIEnv(config)
+    world_parameters = dict(carlatown='Town01', traffic_count=0, pedestrian_count=0, weather='Random')
 
-obs = env.set_scenario('egodriving', world_parameters=world_parameters, sensors=sensors_dict)
+    obs = env.set_scenario('egodriving', world_parameters=world_parameters, sensors=sensors_dict)
 
-dim = (scale*def_res.width, scale*def_res.height)
-width = np.sum([sensors_dict[sns]['resolution'].width*scale for sns in sensors_dict if sensors_dict[sns]['sensor_type']=='camera'])
-height = np.max([sensors_dict[sns]['resolution'].height*scale for sns in sensors_dict if sensors_dict[sns]['sensor_type']=='camera'])
-full_res = Resolution(width, height)
+    dim = (scale*def_res.width, scale*def_res.height)
+    width = np.sum([sensors_dict[sns]['resolution'].width*scale for sns in sensors_dict if sensors_dict[sns]['sensor_type']=='camera'])
+    height = np.max([sensors_dict[sns]['resolution'].height*scale for sns in sensors_dict if sensors_dict[sns]['sensor_type']=='camera'])
+    full_res = Resolution(width, height)
 
-action = [0.0, 0.0]
-obs, reward, done, info = env.step(action)
+    action = [0.0, 0.0]
+    obs, reward, done, info = env.step(action)
 
 def reset_frames():
     return {'images':[], 'coords': [], 'actions': []}
 frames = reset_frames()
 
 videos_per_trajectory = 10
-video_length = 5
+video_length = 1000
 
 def get_save_name(index, mode='video', ext='pt'):
     return os.path.join(args.save_dir, f'{mode}_{index}.{ext}')
@@ -113,5 +114,6 @@ for i in range(0, video_length*videos_per_trajectory):
         print(f'generated {video_length} frames in {time.time()-walltime} seconds')
         walltime = time.time()
 
-print(env.end_simulation())
-env.close()
+with timeout(600):
+    print(env.end_simulation())
+    env.close()
