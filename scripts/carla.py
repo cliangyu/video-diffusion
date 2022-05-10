9f4f1b71710cd71000a98dff2000c8fc92463896
William Harvey <wsgh@submit-ml.cs.ubc.ca>
2022-05-09 17:34:29 -0700

    make carla town configurable in script

--- scripts/carla.py
+++ scripts/carla.py
@@ -35,6 +35,7 @@ with timeout(seconds=1800):
     parser = argparse.ArgumentParser()
     parser.add_argument('save_dir', type=str, help="Where to save the generated videos/coords.")
     parser.add_argument('--port', type=int, default=5555, help="5555 is the server with other cars. Currently there are no other servers")
+    parser.add_argument('--town', type=str, default='Town01')
     args = parser.parse_args()
 
     sensors_dict = {
@@ -58,7 +59,7 @@ with timeout(seconds=1800):
     server_ip = "simulate.inverted.ai"
     config.zmq_server_address = f"{server_ip}:{args.port}"
     env = IAIEnv(config)
-    world_parameters = dict(carlatown='Town01', traffic_count=0, pedestrian_count=0, weather='Random')
+    world_parameters = dict(carlatown=args.town, traffic_count=0, pedestrian_count=0, weather='Random')
 
     obs = env.set_scenario('egodriving', world_parameters=world_parameters, sensors=sensors_dict)
 
