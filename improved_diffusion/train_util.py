5d1dd215cd40bcf61828ed2c73d7a0af749c0881
Will Harvey <williamharvey08@gmail.com>
2022-05-10 23:20:31 -0700

    fixes for distributed training

--- improved_diffusion/train_util.py
+++ improved_diffusion/train_util.py
@@ -430,23 +430,23 @@ class TrainLoop:
             logger.logkv("lg_loss_scale", self.lg_loss_scale)
 
     def save(self):
-        postfix = "latest" if self._args.save_latest_only else f"{(self.step):06d}"
-        to_save = {bf.join(get_blob_logdir(self._args), f"opt_{postfix}.pt"): self.opt.state_dict()}
-
-        # vmnote: make dir if it doesn't exist
-        Path(get_blob_logdir(self._args)).mkdir(parents=True, exist_ok=True)
-
-        for rate, params in zip([0, *self.ema_rate],
-                                [self.master_params, *self.ema_params]):
-            filename = f"ema_{rate}_{postfix}.pt" if rate else f"model_{postfix}.pt"
-            filepath = bf.join(get_blob_logdir(self._args), filename)
-            to_save[filepath] = {
-                "state_dict": self._master_params_to_state_dict(params),
-                "config": self._args.__dict__,
-                "step": self.step
-            }
-
         if dist.get_rank() == 0:
+            postfix = "latest" if self._args.save_latest_only else f"{(self.step):06d}"
+            to_save = {bf.join(get_blob_logdir(self._args), f"opt_{postfix}.pt"): self.opt.state_dict()}
+
+            # vmnote: make dir if it doesn't exist
+            Path(get_blob_logdir(self._args)).mkdir(parents=True, exist_ok=True)
+
+            for rate, params in zip([0, *self.ema_rate],
+                                    [self.master_params, *self.ema_params]):
+                filename = f"ema_{rate}_{postfix}.pt" if rate else f"model_{postfix}.pt"
+                filepath = bf.join(get_blob_logdir(self._args), filename)
+                to_save[filepath] = {
+                    "state_dict": self._master_params_to_state_dict(params),
+                    "config": self._args.__dict__,
+                    "step": self.step
+                }
+
             for path in to_save:
                 # backup previous
                 if os.path.exists(path) and self._args.save_latest_only:
@@ -701,6 +701,8 @@ def find_resume_checkpoint(args):
     """
     If there are checkpoints saved in get_blob_logdir(), will return the latest one
     """
+    if not args.resume_id:
+        return
     logdir = get_blob_logdir(args)
     print('looking in', logdir)
     if not os.path.exists(logdir):
